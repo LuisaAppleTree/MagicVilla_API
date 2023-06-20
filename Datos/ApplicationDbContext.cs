e82b2e7665d34ab3b959e3cdfacad51e31312068
Luisa Manzano <115746809+LuisaAppleTree@users.noreply.github.com>
2023-06-20 13:09:31 -0600

    API

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using MagicVilla_API.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Nombre = "Villa Francesa",
+                    Detalle = "Villa con viñedos en Touluse",
+                    ImagenUrl = "",
+                    Ocupantes = 12,
+                    MetrosCuadrados = 70,
+                    Tarifa = 500,
+                    Amenidad = "Equitación, cata de vinos",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now
+                },
+                  new Villa()
+                  {
+                      Id = 2,
+                      Nombre = "Villa Mediterranea",
+                      Detalle = "Villa con vista al cristalino mar",
+                      ImagenUrl = "",
+                      Ocupantes = 8,
+                      MetrosCuadrados = 40,
+                      Tarifa = 800,
+                      Amenidad = "Nado en aguas heladas, bajo el cálido sol de la costa",
+                      FechaCreacion = DateTime.Now,
+                      FechaActualizacion = DateTime.Now
+                  }
+            );
+        }
+    }
+}
---
+++ Migrations/20230620172447_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "Equitación, cata de vinos", "Villa con viñedos en Touluse", new DateTime(2023, 6, 20, 11, 24, 47, 547, DateTimeKind.Local).AddTicks(5468), new DateTime(2023, 6, 20, 11, 24, 47, 547, DateTimeKind.Local).AddTicks(5455), "", 70.0, "Villa Francesa", 12, 500.0 },
+                    { 2, "Nado en aguas heladas, bajo el cálido sol de la costa", "Villa con vista al cristalino mar", new DateTime(2023, 6, 20, 11, 24, 47, 547, DateTimeKind.Local).AddTicks(5471), new DateTime(2023, 6, 20, 11, 24, 47, 547, DateTimeKind.Local).AddTicks(5470), "", 40.0, "Villa Mediterranea", 8, 800.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
